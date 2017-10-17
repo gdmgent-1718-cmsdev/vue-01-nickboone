7db42b7711be8a7a212744de22ecc5801560b1b7
nickboone <nick.boone@student.arteveldehs.be>
2017-10-17 11:58:05 +0200

    Opdracht vue-01

---
+++ 1718-cmsdev-code/vue/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+<div>
+  <ul v-if="posts && posts.length">
+    <li v-for="post of posts">
+      <p><strong>{{post.title}}</strong></p>
+      <p>{{post.body}}</p>
+    </li>
+  </ul>
+
+  <ul v-if="errors && errors.length">
+    <li v-for="error of errors">
+      {{error.message}}
+    </li>
+  </ul>
+</div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data () {
+    return {
+      posts: [],
+      errors: []
+    }
+  },
+
+  // Fetches posts when the component is created.
+  created () {
+    axios.get(`http://cmsdev.localhost/node/5?_format=hal_json`)
+    .then(response => console.log(response.data.body[0].value))
+
+    // async / await version (created() becomes async created())
+    //
+    // try {
+    //   const response = await axios.get(`http://cmsdev.localhost`)
+    //   this.posts = response.data
+    // } catch (e) {
+    //   this.errors.push(e)
+    // }
+  }
+}
+</script>
